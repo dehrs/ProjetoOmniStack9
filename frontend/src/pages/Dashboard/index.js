7ffc3fe2ad36a43c963bf76e6ff63c5bc906304a
Débora Rodrigues <deborarsousa@gmail.com>
2020-01-06 16:57:54 -0300

    Feitas as páginas do frontend e implementada solicitações do usuário em tempo real

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,13 +1,32 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useState, useMemo } from "react";
+import { Link } from "react-router-dom";
+import socketio from "socket.io-client";
 import api from "../../services/api";
 
+import "./styles.css";
+
 export default function Dashboard() {
+  const user_id = localStorage.getItem("user");
+
+  const socket = useMemo(
+    () =>
+      socketio("http://localhost:3333", {
+        query: { user_id }
+      }),
+    [user_id]
+  );
+
   const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    socket.on("booking_request", data => {
+      setRequests([...requests, data]);
+    });
+  }, [requests, socket]);
+
   useEffect(() => {
     async function loadSpots() {
-      const user_id = localStorage.getItem("user");
-
       const response = await api.get("/dashboard", {
         headers: { user_id }
       });
@@ -16,19 +35,38 @@ export default function Dashboard() {
     }
 
     loadSpots();
-  }, []);
+  }, [user_id]);
 
   return (
     <>
-      <ul className="spot-list">
-        {spots.map(spot => (
-          <li key={spot._id}>
-            <header />
-            <strong>{spot.company}</strong>
-            <span>{spot.price}</span>
+      <ul className="notifications">
+        {requests.map(request => (
+          <li key={request._id}>
+            <p>
+              <strong>{request.user.email}</strong> está solicitando uma reserva
+              em <strong>{request.spot.company}</strong> para a data:{" "}
+              <strong> {request.date}</strong>
+            </p>
+
+            <button className="accept">Aceitar</button>
+            <button className="reject">Rejeitar</button>
           </li>
         ))}
       </ul>
+
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$ ${spot.price}/dia` : "Gratuito"}</span>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/new">
+        <button className="btn">Cadastrar Novo Spot</button>
+      </Link>
     </>
   );
 }
